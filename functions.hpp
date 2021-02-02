84ce4798afff3f0881964a94b858a4fdd2e1ccf9
djblackett <49618491+djblackett@users.noreply.github.com>
2021-02-02 19:52:28 -0400

    Create functions.hpp

---
+++ functions.hpp
@@ -0,0 +1,6 @@
+void draw(char board[]);
+void takeTurn(char board[], int turnCount);
+void update(char board[]);
+bool whoseTurnIsIt(int turnCount);
+bool isWinner(char board[]);
+bool isBoardFull(char board[]);
